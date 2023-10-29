fb0d03f8fc20fe75b7a965b1299b4bb42da71969
sergey <mamban1997@gmail.com>
2023-10-29 17:44:15 +0400

    init

---
+++ src/main/java/com/example/friendsletter/FriendsletterApplication.java
@@ -0,0 +1,13 @@
+package com.example.friendsletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsletterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsletterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/friendsletter/FriendsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.friendsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsletterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
