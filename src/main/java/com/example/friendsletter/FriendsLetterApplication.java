29e1e099b1c2442f96239e55670e7937c2d3ea39
sergey <mamban1997@gmail.com>
2023-11-03 01:35:34 +0400

    update todo

--- src/main/java/com/example/friendsletter/FriendsLetterApplication.java
+++ src/main/java/com/example/friendsletter/FriendsLetterApplication.java
@@ -11,15 +11,17 @@ public class FriendsLetterApplication {
         //todo add local compressed message store
         //todo tests
         //todo message qr code
-        //todo list public messages
         //todo redirect after post
         //todo rate limiter
-        //todo swagger
-        //todo api endpoints
         //todo javadoc
-        //todo cache!!
         //todo 404 handle
+        //todo different dto for create message
+        //todo docker and redis and postgres
 
+        //list public messages
+        //cache!!
+        //swagger
+        //api endpoints
         //local timezone in view
         //message stats
         //copy letter button
