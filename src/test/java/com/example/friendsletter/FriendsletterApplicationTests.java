b626acbb88a4d188f86f274fea8bf091e6e5b40f
sergey <mamban1997@gmail.com>
2023-11-25 22:02:40 +0400

    fix property

--- src/test/java/com/example/friendsletter/FriendsletterApplicationTests.java
+++ src/test/java/com/example/friendsletter/FriendsletterApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.friendsletter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
+@ActiveProfiles("test")
 class FriendsletterApplicationTests {
 
     @Test
